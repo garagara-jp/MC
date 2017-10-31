eda7d094fd17e264c533381d34b71abad8ec8302
garagara-denx <sinfu000@gmail.com>
2017-11-01 04:04:16 +0900

    MoneyEmitterを作成

--- Assets/MyApp/Scripts/Enemy/MoneyEmitter.cs
+++ Assets/MyApp/Scripts/Enemy/MoneyEmitter.cs
@@ -8,5 +8,38 @@ using UnityEngine;
 /// </summary>
 public class MoneyEmitter : MonoBehaviour
 {
+    EnemyStatusModel enemyStatusModel;
 
+    [SerializeField]
+    GameObject moneyPrefab;
+
+    private void Start()
+    {
+        enemyStatusModel = GetComponent<EnemyStatusModel>();
+        if (moneyPrefab == null)
+            moneyPrefab = GameObject.FindWithTag("Money");
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (enemyStatusModel.IsDead)
+        {
+            // moneyを生成
+            GameObject money = Instantiate(moneyPrefab, transform.position, transform.rotation);
+
+            // moneyのstatusを設定
+            var moneyModel = money.GetComponent<MoneyStatusModel>();
+            moneyModel.MoneyValue = enemyStatusModel.EnemyMoney;
+
+            // ランダムな方向に射出
+            var targetVec = new Vector2((Random.Range(-1, 1) >= 0) ? 1 : -1, Random.Range(-1, 5));
+            Rigidbody2D moneyRb = money.GetComponent<Rigidbody2D>();
+            moneyRb.velocity += targetVec;
+        }
+    }
 }

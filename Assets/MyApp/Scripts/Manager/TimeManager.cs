bc411c842cd7d1d3320b7fc6bbcad12c8ae02166
garagara-denx <sinfu000@gmail.com>
2017-11-01 02:19:20 +0900

    TimeManagerを作成

--- Assets/MyApp/Scripts/Manager/TimeManager.cs
+++ Assets/MyApp/Scripts/Manager/TimeManager.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeManager : MonoBehaviour {
+/// <summary>
+/// GameManagerにアタッチ
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    private float gameStartTime;
+    private float elapsedTime;
+    private float remainingTime;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float GameStartTime { get { return gameStartTime; } }
+    public float ElapsedTime { get { return elapsedTime; } }
+    public float RemainingTIme { get { return remainingTime; } }
+
+    private void Update()
+    {
+        elapsedTime = Time.time - gameStartTime;
+        remainingTime -= elapsedTime;
+    }
+
+    public void StartGame(float limitTime)
+    {
+        gameStartTime = Time.time;
+        remainingTime = limitTime;
+    }
+
+
 }

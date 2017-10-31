d9393b54fd2790fc40b5e6b8f60a31c2aa6acd07
garagara-denx <sinfu000@gmail.com>
2017-11-01 03:06:34 +0900

    ButtleManagerを作成

---
+++ Assets/MyApp/Scripts/Manager/ButtleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtleManager : MonoBehaviour
+{
+    TimeManager timeManager;
+
+    [SerializeField]
+    private bool gameStart = false;
+    [SerializeField]
+    private float limitTime = 180;
+    [SerializeField]
+    private bool pause;
+
+    private void Start()
+    {
+        timeManager = GetComponent<TimeManager>();
+    }
+
+    private void Update()
+    {
+        if (gameStart)
+        {
+            timeManager.StartGame(limitTime);
+            gameStart = false;
+        }
+
+        if (pause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}

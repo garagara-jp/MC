c1b8f6245bef28831e6c37ff59a7da8fb35af53f
garagara-denx <sinfu000@gmail.com>
2017-11-02 07:34:15 +0900

    ItemTrade画面をEspで抜けれるように修正

--- Assets/MyApp/Scripts/Dealer/TradeWindowViewer.cs
+++ Assets/MyApp/Scripts/Dealer/TradeWindowViewer.cs
@@ -11,10 +11,20 @@ public class TradeWindowViewer : MonoBehaviour
     DealerStatusModel dealerStatusModel;
 
     [SerializeField]
+    private SpriteRenderer popup;
 
     private void Start()
     {
         dealerStatusModel = GetComponent<DealerStatusModel>();
+        popup.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (dealerStatusModel.isShowWindow && Input.GetKeyDown(KeyCode.Escape))
+        {
+            dealerStatusModel.isShowWindow = false;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D col)
@@ -25,8 +35,18 @@ public class TradeWindowViewer : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            popup.enabled = false;
+        }
+    }
+
     private void ShowWindowCmd()
     {
+        popup.enabled = true;
+
         if (Input.GetKeyDown(KeyCode.I))
         {
             dealerStatusModel.isShowWindow = true;

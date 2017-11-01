10f70bcec7a840266468dd4c9a7278dde537e06e
garagara-denx <sinfu000@gmail.com>
2017-11-02 07:35:03 +0900

    Game全体のStatusを管理するモデルを追加

---
+++ Assets/MyApp/Scripts/Manager/GameStatusModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatusModel : MonoBehaviour
+{
+    [SerializeField]
+    private float limitTime = 180;
+    [SerializeField]
+    private float enemySpawnInterval = 1;
+
+    public bool IsPause { get; set; }
+    public bool IsGameStarted { get; set; }
+    public float LimitTime { get; set; }
+    public bool DoSpawn { get; set; }
+    public float EnemySpawnInterval { get; set; }
+
+    private void Awake()
+    {
+        LimitTime = limitTime;
+        EnemySpawnInterval = enemySpawnInterval;
+        IsPause = false;
+        IsGameStarted = false;
+        DoSpawn = false;
+    }
+}

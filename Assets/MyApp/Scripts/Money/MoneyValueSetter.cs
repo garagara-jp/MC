7ad988fc5be5476c0a841a1d150d9eae8db64ddb
garagara-denx <sinfu000@gmail.com>
2017-11-01 16:43:21 +0900

    HaveMoneyインターフェースを継承したMoneyValueSetterを追加

---
+++ Assets/MyApp/Scripts/Money/MoneyValueSetter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyValueSetter : MonoBehaviour, IHaveMoney
+{
+    MoneyStatusModel moneyStatusModel;
+
+    private void Start()
+    {
+        moneyStatusModel = GetComponent<MoneyStatusModel>();
+    }
+
+    public void SetMoneyValue(float moneyValue)
+    {
+        moneyStatusModel.MoneyValue = moneyValue;
+    }
+}

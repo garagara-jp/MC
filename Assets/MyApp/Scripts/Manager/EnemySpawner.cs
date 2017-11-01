dfe963f8113c8a7117a569ae6508cd21407cc963
garagara-denx <sinfu000@gmail.com>
2017-11-01 20:32:10 +0900

    EnemySpawnerを作成

---
+++ Assets/MyApp/Scripts/Manager/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GameManagerにアタッチ
+/// </summary>
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private Transform spawnPlace;
+    [SerializeField]
+    private bool doSpawn = true;
+    [SerializeField]
+    private float spawnInterval = 1f;   //秒
+
+    private void Start()
+    {
+        // コルーチンを設定
+        StartCoroutine(SpawnEnemy(spawnInterval));
+    }
+
+    private IEnumerator SpawnEnemy(float second)
+    {
+        // ループ
+        while (doSpawn)
+        {
+            var enemy = Instantiate(enemyPrefab, spawnPlace.position, spawnPlace.rotation);
+            yield return new WaitForSeconds(second);
+        }
+    }
+}

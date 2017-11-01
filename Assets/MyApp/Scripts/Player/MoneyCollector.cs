002d6c4fc6384b526a54fe2fcca0387be4c27649
garagara-denx <sinfu000@gmail.com>
2017-11-01 20:05:40 +0900

    MoneyCollectorを作成

--- Assets/MyApp/Scripts/Player/MoneyCollector.cs
+++ Assets/MyApp/Scripts/Player/MoneyCollector.cs
@@ -8,5 +8,20 @@ using UnityEngine;
 /// </summary>
 public class MoneyCollector : MonoBehaviour
 {
+    PlayerStatusModel playerStatusModel;
 
+    private void Start()
+    {
+        playerStatusModel = GetComponent<PlayerStatusModel>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        var getableMoney = col.gameObject.GetComponent<IGetableMoney>();
+        if (getableMoney != null)
+        {
+            playerStatusModel.PlayerMoney += getableMoney.GetMoneyValue();
+            Destroy(col.gameObject);
+        }
+    }
 }

3d67133594944a7f97fc92bfc6bd91aabf632ed4
garagara-denx <sinfu000@gmail.com>
2017-11-01 20:04:55 +0900

    GetableMoneyインターフェースを継承したクラスを作成

---
+++ Assets/MyApp/Scripts/Money/MoneyValueGetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moneyにアタッチ
+/// MoneyのValueをインターフェース越しに渡す
+/// </summary>
+public class MoneyValueGetter : MonoBehaviour, IGetableMoney
+{
+    MoneyStatusModel moneyStatusModel;
+
+    private void Start()
+    {
+        moneyStatusModel = GetComponent<MoneyStatusModel>();
+    }
+
+    public float GetMoneyValue()
+    {
+        return moneyStatusModel.MoneyValue;
+    }
+}

354f7329fe6878e55224ed43a739ed8cb8b23495
garagara-denx <sinfu000@gmail.com>
2017-11-01 19:09:19 +0900

    EnemyDeathManagerを作成

---
+++ Assets/MyApp/Scripts/Enemy/EnemyDeathManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemyにアタッチ
+/// HPが0になったときの処理を管理
+/// </summary>
+public class EnemyDeathManager : MonoBehaviour
+{
+    EnemyStatusModel enemyStatusModel;
+
+    private void Start()
+    {
+        enemyStatusModel = GetComponent<EnemyStatusModel>();
+    }
+
+    private void Update()
+    {
+        if (enemyStatusModel.IsDead && !enemyStatusModel.IsHaveMoney)
+        {
+            Destroy(gameObject);
+            Debug.Log("死にました");
+        }
+    }
+}

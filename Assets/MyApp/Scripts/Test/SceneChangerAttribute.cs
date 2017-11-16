4c34814c4df8b9bef1dbcb26529e75054d1c976d
garagara-denx <sinfu000@gmail.com>
2017-11-17 03:42:30 +0900

    Revert "Test用のファイルをTestディレクトリに隔離してAssets内を整理"
    This reverts commit 9fb641b3f0a4ba196175b43682c0fcf2ff3d4034.

--- Assets/MyApp/Scripts/Test/SceneChangerAttribute.cs
+++ Assets/MyApp/Scripts/Test/SceneChangerAttribute.cs
@@ -1,6 +1,4 @@
-﻿// 参照: https://qiita.com/keel/items/11ec62aff184a3a2cccc
-
-using UnityEngine;
+﻿using UnityEngine;
 #if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
@@ -8,7 +6,7 @@ using System.Linq;
 using UnityEditor;
 #endif
 
-public class SceneChangerAttribute : PropertyAttribute { }
+class SceneChangerAttribute : PropertyAttribute { }
 [CustomPropertyDrawer(typeof(SceneChangerAttribute))]
 public class SceneChangerEditor : PropertyDrawer
 {

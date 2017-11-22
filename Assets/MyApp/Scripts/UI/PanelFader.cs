e87af37b48a99c33a0eb7d3a2e14aec985ac90bd
garagara-denx <sinfu000@gmail.com>
2017-11-22 15:31:12 +0900

    TitleにFadeを追加

---
+++ Assets/MyApp/Scripts/UI/PanelFader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PanelFader : MonoBehaviour
+{
+    private float alfa = 1;
+    [SerializeField]
+    private float speed = 0.01f;
+    [SerializeField]
+    private GameObject panel;
+    private Image panelImage;
+    private float red, green, blue;
+
+    void Start()
+    {
+        if (!panel.activeInHierarchy)
+        {
+            panel.SetActive(true);
+        }
+
+        panelImage = panel.GetComponent<Image>();
+        red = panelImage.color.r;
+        green = panelImage.color.g;
+        blue = panelImage.color.b;
+    }
+
+    void Update()
+    {
+        panelImage.color = new Color(red, green, blue, alfa);
+        alfa -= speed;
+        if (alfa <= 0)
+            panel.SetActive(false);
+    }
+}

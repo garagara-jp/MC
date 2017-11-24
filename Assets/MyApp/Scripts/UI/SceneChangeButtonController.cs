39d05818bcdfa9622c3b6b02378d2636cdb1beb4
garagara-denx <sinfu000@gmail.com>
2017-11-24 16:38:37 +0900

    SceneChangeButtonControllerを実装

---
+++ Assets/MyApp/Scripts/UI/SceneChangeButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeButtonController : MonoBehaviour
+{
+    Button btn;
+
+    //どんな名前でもいいのでstring型のフィールドに付ける
+    [SerializeField, SceneChangerAttribute]
+    string nextScene;
+
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClickFunction);
+    }
+
+    void OnClickFunction()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
